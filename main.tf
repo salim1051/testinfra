982446f20031202750b8dad27bcba390f88947c6
salim1051 <salim1051@gmail.com>
2026-08-22 17:49:35 +0530

    changed RG NAME

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.1.0"
     }
   }
+ backend "azurerm" {
+    resource_group_name  = "salim_rg1"                              # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "storagetestpipeline"                          # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "storagecontainer"                   # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "terraform.tfstate"                    # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 
 provider "azurerm" {
@@ -12,6 +18,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg1" {
-  name     = "salim_rg5"
+  name     = "salim_rg6"
   location = "centralindia"
 }
